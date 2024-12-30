21c9425c2c51252246990dce1cca1b7433cd4f17
serendipity019 <navagio09@gmail.com>
2024-12-31 00:48:03 +0200

    Akoma to provlima den exei lithi

--- src/Projects/ProjectFive.java
+++ src/Projects/ProjectFive.java
@@ -18,7 +18,7 @@ public class ProjectFive {
             try (Scanner sc = new Scanner(System.in)) {
                 System.out.println("Αν θέλετε να κάνετε κράτηση επιλέξτε 1\n" + "Αν θέλετε να κάνετε ακύρωση επιλέξτε 2.");
                 bookOrCancel = sc.nextInt();
-                sc.close();
+
             } catch (NumberFormatException e) {
                 System.err.println("Εισάγετε αριθμό. 1 για κράτηση ή 2 για ακύρωση θέσης");
             }
@@ -34,44 +34,38 @@ public class ProjectFive {
                 System.out.println("π.χ για 3η στήλη, 2η σειρά εισάγετε C2.");
                 seat = sc.nextLine();
                 if (seat.length() != 2) {
-                    System.out.println("Δεν εισάγατε κατάληλη θέση, προσπαθήστε ξανά!");
-                    System.out.println("Επιλέξτε θέση απο A εώς L (Αγγλικοί κεφαλαίοι χαρακτήρες) για στήλη και απο 1 εώς 30 για την σειρά. ");
-                    seat = sc.nextLine();
-                } else {
-                    column = Character.toUpperCase(seat.charAt(0));
-                    row = Character.getNumericValue(seat.charAt(1));
-                    if ((Integer.valueOf(column % 65) > 11) || (Integer.valueOf(column % 65) < 1) || (row > 30 || row < 1)) {
-                        System.out.println("Δεν εισάγατε κατάληλη θέση, προσπαθήστε ξανά!");
-                        System.out.println("Επιλέξτε θέση απο A εώς L (Αγγλικοί κεφαλαίοι χαρακτήρες) για στήλη και απο 1 εώς 30 για την σειρά. ");
-                        seat = sc.nextLine();
-                    } else {
-                        break;
-                    }
+                    throw new IllegalArgumentException("Δεν εισάγατε κατάληλη θέση, προσπαθήστε ξανά!");
+                }
+                column = Character.toUpperCase(seat.charAt(0));
+                row = Character.getNumericValue(seat.charAt(1));
+                if ((column > 'L') || (column < 'A') || (row > 30 || row < 1)) {
+                    throw new IllegalArgumentException("Δεν εισάγατε κατάληλη θέση, προσπαθήστε ξανά!");
+
                 }
 
-            } catch (Exception e) {
-                System.err.println("Λάθος! " + e.getMessage());
+                break;
+            } catch (IllegalArgumentException e){
+                System.err.println("Λάθος Εισαγωγή! " + e.getMessage());
+            }catch (Exception e) {
+                System.err.println("Ουπς κάτι πήγε στραβά! " + e.getMessage());
             }
         } while (true);
-        column = Character.toUpperCase(seat.charAt(0));
-        row = Character.getNumericValue(seat.charAt(1));
-
 
         if (bookOrCancel == 1) {
             book(column, row);
-        } else if (bookOrCancel == 2) {
+        } else{
             cancel(column, row);
         }
 
 
-        doubleTablePrinter(theaterTable);
+       // doubleTablePrinter(theaterTable);
     }
 
 
 
 
     private static void book(char column, int row){
-        int colNum = Integer.valueOf(column % 65);
+        int colNum = column % 65;
         if (theaterTable[colNum][row-1]) {
             System.out.println("η θέση: "+ column + row + " είναι κλεισμένη." );
         } else {
@@ -81,7 +75,7 @@ public class ProjectFive {
     }
 
     private static void cancel(char column, int row){
-        int colNum = Integer.valueOf(column % 65);
+        int colNum = column % 65;
         if (theaterTable[colNum][row-1]) {
             theaterTable[colNum][row-1] = false;
             System.out.println("η θέση: "+ column + row + " ακυρώθηκε επιτυχώς." );
